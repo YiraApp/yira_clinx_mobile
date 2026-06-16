2ce29994ea9b131e86b40fe5f0037dc0d8370350
Challa Bhargava <131694399+Challabhargavanarasimha@users.noreply.github.com>
2026-06-16 15:47:22 +0530

    identified the bundle identifier

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.yira.yiraclinx"
+    namespace = "com.paccore.yiralife"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.yira.yiraclinx"
+        applicationId = "com.paccore.yiralife"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
