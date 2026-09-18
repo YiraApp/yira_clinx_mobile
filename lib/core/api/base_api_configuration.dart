3a8d81b0ac44597c9ab9da9a9dbcfc9e0d5f7069
Teja <teja.ch@yira.ai>
2026-09-18 10:04:39 +0530

    fix

--- lib/core/api/base_api_configuration.dart
+++ lib/core/api/base_api_configuration.dart
@@ -3,8 +3,8 @@ class EnvironmentService {
   static void setEnvironment(Environment env) => _currentEnv = env;
 
   // Local backend server URL for local development (matches local backend on port 5000)
-  // - 192.168.68.138:5000 enables connectivity for physical devices on Wi-Fi and emulators/simulators
-  static const String localHostIp = "192.168.0.5";
+  // - 192.168.68.61:5000 enables connectivity for physical devices on Wi-Fi and emulators/simulators
+  static const String localHostIp = "192.168.68.61";
   static const String localPort = "5000";
   static String? _customBaseUrl;
 
